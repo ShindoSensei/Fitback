faee2c15cbc6f1a4e8cd00d491226a536af2b27f
Ang Cheng Siew <johnacsyen@gmail.com>
2017-03-30 11:02:27 +0800

    mqtt faker controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'faker', to: 'faker#fake', as: 'faker'
+  get 'fakeoff', to: 'faker#fakeOff', as: 'fakeoff'
 
   devise_for :users
   root 'static#homepage'
 
   resources :participants
+  resources :trainees
   resources :wearables
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/jobs/fake_mqtt_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FakeMqttJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
